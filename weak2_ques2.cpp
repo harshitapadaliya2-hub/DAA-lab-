1052d3723f194d5fd8cdc6b87696f91765523083
harshitapadaliya2-hub <harshitapadaliya2@gmail.com>
2026-03-13 20:53:07 +0530

    Create weak2_ques2.cpp

---
+++ weak2_ques2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;  
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        int key;
+        cin >> key;
+
+        int low = 0, high = n - 1;
+        int comparisons = 0;
+        bool found = false;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            comparisons++;
+
+            if (arr[mid] == key) {
+                found = true;
+                break;
+            }
+            else if (key < arr[mid]) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        if (found)
+            cout << "Present " << comparisons << endl;
+        else
+            cout << "Not Present " << comparisons << endl;
+    }
+
+    return 0;
+}
