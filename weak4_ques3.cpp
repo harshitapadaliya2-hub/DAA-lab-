47ff8d3e9aff2590beebaf176dbe2036dc4edb5e
harshitapadaliya2-hub <harshitapadaliya2@gmail.com>
2026-03-14 19:41:07 +0530

    Create weak4_ques3.cpp

---
+++ weak4_ques3.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int partition(vector<int>& arr, int l, int r, int x) {
+    int i;
+    for (i = l; i < r; i++)
+        if (arr[i] == x)
+            break;
+    swap(arr[i], arr[r]);
+    i = l;
+    for (int j = l; j < r; j++) {
+        if (arr[j] <= x) {
+            swap(arr[i], arr[j]);
+            i++;
+        }
+    }
+    swap(arr[i], arr[r]);
+    return i;
+}
+
+int kthSmallest(vector<int>& arr, int l, int r, int k) {
+    if (k > 0 && k <= r - l + 1) {
+
+        int n = r - l + 1;
+        int i;
+        vector<int> median((n + 4) / 5);
+        for (i = 0; i < n / 5; i++) {
+            sort(arr.begin() + l + i * 5, arr.begin() + l + i * 5 + 5);
+            median[i] = arr[l + i * 5 + 2];
+        }
+        if (i * 5 < n) {
+            sort(arr.begin() + l + i * 5, arr.begin() + l + i * 5 + (n % 5));
+            median[i] = arr[l + i * 5 + (n % 5) / 2];
+            i++;
+        }
+        int medOfMed = (i == 1) ? median[0]
+                                : kthSmallest(median, 0, i - 1, i / 2);
+
+        int pos = partition(arr, l, r, medOfMed);
+        if (pos - l == k - 1)
+            return arr[pos];
+        if (pos - l > k - 1)
+            return kthSmallest(arr, l, pos - 1, k);
+        return kthSmallest(arr, pos + 1, r, k - pos + l - 1);
+    }
+    return INT_MAX;
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        int k;
+        cin >> k;
+        if (k > n || k <= 0)
+            cout << "not present" << endl;
+        else
+            cout << kthSmallest(arr, 0, n - 1, k) << endl;
+    }
+}
